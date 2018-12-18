95e746cbf758b46e6ada478b2505e1e2ef9bdd15
David <sullydas@gmail.com>
2018-12-18 10:43:56 -0600

    initial commit

---
+++ NPCGEN/Traits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Traits
+{
+    public class Advantage{
+
+        public String name { get { return name; } }
+        public String description { get { return description; } }
+        public int points { get { return points; } }
+
+
+        public Advantage(String n, String d, int p)
+        {
+            name = n;
+            description = d;
+            points = p;
+
+        }
+    }
+}
